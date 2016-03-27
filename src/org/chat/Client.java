ebf98887c977bfba39c2be7889062a727b034ac5
Dmitry Avdeev <Dzmitry_Avdeev@epam.com>
2016-03-27 16:27:49 +0300

    rename main class

--- src/org/chat/Client.java
+++ src/org/chat/Client.java
@@ -1,40 +1,31 @@
 package org.chat;
 import java.net.*;
 import java.io.*;
-import java.util.Random;
-public class Main {
-    static Random gen = new Random();
-    static String randomNick(){
-        return String.valueOf(gen.nextInt());
-    }
+
+public class Client {
+
     static BufferedReader in;
     public static void main(String[] args) {
-        String nick = randomNick();
-        if(args.length >= 1){
-            nick = args[0];
-        }
         String host = "localhost";
-        if(args.length >= 2){
+        if(args.length >= 1){
             host = args[0];
         }
         int port = 7777;
-        if(args.length >= 3){
-            port = Integer.parseInt(args[2]);
+        if(args.length >= 2){
+            port = Integer.parseInt(args[1]);
         }
         try (
-                Socket socket = new Socket(host, port);
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+            Socket socket = new Socket(host, port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
         ) {
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            System.out.print("xxx");
-            (new ListenFromServer()).start();
-            System.out.print("xxx");
+            Thread listener = new ListenFromServer();
+            listener.start();
             String userInput;
-            String serverMessage;
             while ((userInput = stdIn.readLine()) != null) {
                 out.println(userInput);
-                if(userInput.equals("close")){
+                if(userInput.equals("server disconnect")){
                     socket.close();
                     System.exit(0);
                 }
@@ -43,13 +34,11 @@ public class Main {
             System.err.println("Don't know about host " + host);
             System.exit(1);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    host);
+            System.err.println("Couldn't get I/O for the connection to " + host);
             System.exit(1);
         }
     }
     static class ListenFromServer extends Thread {
-
         public void run() {
             while(true) {
                 try {
